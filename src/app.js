e216b14348569b65f3a6685496e91446803b88b7
Matheus Queiroz <matheus@queirozavaliacoes.com.br>
2022-10-11 18:51:36 -0300

    cuida

--- src/app.js
+++ src/app.js
@@ -1,18 +1,53 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
+const characters = [
+  {
+    id: 0,
+    name: 'aaa'
+  },
+  {
+    id: 1,
+    name: 'bbb'
+  },
+  {
+    id: 0,
+    name: 'ccc'
+  }
+]
 app.get('/', (req, res) => {
   res.json({
-    test: "a"
+    test: 'a'
   })
 })
 
-export default app;
+app.get('/characters', (req, res) => {
+  res.json({
+    test: 'a'
+  })
+})
+
+app.post('/characters', (req, res) => {
+  res.json({
+    test: 'a'
+  })
+})
+
+app.get('/characters/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    characters: characters.filter(
+      character => String(character.id) === String(id)
+    )
+  })
+})
+
+export default app
